735f697023225f0d620a667de36f0c4999fb54f5
peeldok <40337551+peeldok@users.noreply.github.com>
2026-08-09 11:30:03 +0900

    Delete main directory

--- main/boot_animation.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void boot_animation_start();
--- main/midi.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void midi_init();
-void midi_queue_button(uint8_t note, bool pressed, uint8_t velocity = 127);
